7397212f80e9c5f8bfcbb3a2ec0d7fbdac830f55
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-10-09 02:21:06 +0300

    all except *

--- serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client1.kt
@@ -1,12 +1,22 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.io.Serializable
 import java.time.LocalDate
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Client1(
+    //@JsonProperty("FIRSTNAME")
     val firstName: String,
+    //@JsonProperty("LASTNAME")
     val lastName: String,
+    //@JsonProperty("MIDDLENAME")
     val middleName: String?,
+    //@JsonProperty("PASSPORTNUMBER")
     val passportNumber: String,
+    //@JsonProperty("PASSPORTSERIAL")
     val passportSerial: String,
+    //@JsonProperty("BIRTHDATE")
     val birthDate: LocalDate
 )
--- serialization/src/test/kotlin/ru/sber/serialization/JsonSerializationTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonSerializationTest.kt
@@ -1,5 +1,6 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -11,6 +12,7 @@ class JsonSerializationTest {
         // given
         val client = Client5()
         val objectMapper = ObjectMapper()
+
 
         // when
         val data = objectMapper.writeValueAsString(client)
@@ -24,6 +26,7 @@ class JsonSerializationTest {
         // given
         val client = Client6()
         val objectMapper = ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
         // when
         val data = objectMapper.writeValueAsString(client)
