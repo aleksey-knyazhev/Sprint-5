79d5f9c5ace624ba55002ae84a26d15f42c57856
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-10-09 04:10:09 +0300

    all

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,20 +1,50 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.Version
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
+
 class JsonCustomClassDeserializer {
+
+//    class Client7Deserializer(private val clazz: Class<T>) : StdDeserializer<Client7>(clazz) {
+//
+//        override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): Client7 {
+//            val value = p?.codec?.readValue(p, clazz)
+//            return if (value == null) Client7.empty() else Client7.just(value)
+//        }
+//    }
+
+//    fun deserialize(parser: JsonParser, deserializer: DeserializationContext?): Client7? {
+//        val codec = parser.codec
+//        val node = codec.readTree<JsonNode>(parser)
+//
+//        val clientSplit = node["client"].toString().split(" ")
+//        return Client7(clientSplit[0], clientSplit[1], clientSplit[2])
+//    }
 
     @Test
     fun `Нобходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
+
+        val module = SimpleModule("CustomCarDeserializer", Version(1, 0, 0, null, null, null))
+        module.addDeserializer(Client7::class.java, CustomClient7Deserializer())
+
         val objectMapper = ObjectMapper()
-            .registerModules(KotlinModule(), JavaTimeModule())
+            .registerModules(KotlinModule(), JavaTimeModule(), module)
+
+
+
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -23,5 +53,17 @@ class JsonCustomClassDeserializer {
         assertEquals("Иван", client.firstName)
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
+    }
+}
+
+// Пример брал отсюда, п. 4.2: https://www.baeldung.com/jackson-object-mapper-tutorial
+// Корректно ли написана конструкция "@JvmOverloads constructor(vc: Class<*>? = null) : StdDeserializer<Client7?>(vc)" - не знаю. Но оно работает
+class CustomClient7Deserializer @JvmOverloads constructor(vc: Class<*>? = null) : StdDeserializer<Client7?>(vc) {
+    override fun deserialize(parser: JsonParser, deserializer: DeserializationContext): Client7 {
+        val codec = parser.codec
+        val node = codec.readTree<JsonNode>(parser)
+
+        val clientSplit = node["client"].toString().replace("\"", "").trim().split(" ")
+        return Client7(clientSplit[1], clientSplit[0], clientSplit[2])
     }
 }
